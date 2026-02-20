764864bf62a1c037bc7d6438491db3edc540bb96
527esilvia-sudo <527Esilvia@frhsd.com>
2026-02-20 10:36:06 -0500

    finished num

--- script.js
+++ script.js
@@ -1,7 +1,57 @@
-function pressNumber(digit){
-console.log(digit)
+// user typing
+let typedNumberText = ''
+// number for calc
+let storedNumber = null
+
+let currentOperator = ''
+
+let historyParts = []
+
+
+function setStatus(message) {
+    document.getElementById('statusLine').textContent = message
 }
 
-function pressOperator(operator){
-    console.log(operator)
+function showSymbol(operator) {
+    if (operator === '*') {
+        return 'x'
+    }
+    if (operator === '/') {
+        return '÷'
+    }
+    if (operator === '+') {
+        return '+'
+    }
+    if (operator === '-') {
+        return '-'
+    }
 }
+
+    function updateScreen() {
+        const display = document.getElementById('displayLine')
+        const history = document.getElementById('historyLine')
+        const status = document.getElementById('statusLine')
+
+        display.textContent = typedNumberText
+    }
+
+
+
+
+
+
+    function pressNumber(digit) {
+        setStatus('')
+       if(typedNumberText === '0'){
+        typedNumberText = digit
+       }
+       else {
+         typedNumberText = typedNumberText + digit
+       }
+        updateScreen()
+
+    }
+
+    function pressOperator(operator) {
+        console.log(operator)
+    }
